1b9e572a0d39aec0d7d68e94a8ecdc72d731becb
Serhii Suprun <ssuprun@macbook.local>
2019-08-12 18:19:42 +0300

    added alias to JOIN

--- Sources/StORM/StORMJoins.swift
+++ Sources/StORM/StORMJoins.swift
@@ -10,14 +10,16 @@
 /// Join Support is under development.
 public struct StORMDataSourceJoin {
 	public var table:			String		= ""
+    public var alias:          String        = ""
 	public var direction:		StORMJoinType
 	public var onCondition:		String		= ""
 
 	public init() {
 		direction = .normal
 	}
-	public init(table: String,  onCondition: String = "", direction: StORMJoinType = .normal) {
+    public init(table: String, alias: String = "", onCondition: String = "", direction: StORMJoinType = .normal) {
 		self.table = table
+        self.alias = alias
 		self.direction = direction
 		self.onCondition = onCondition
 	}
